aea14249101e53f7152da9ed2778a84450868456
Marquis Abah <marquis.abah@shuttlers.ng>
2024-04-20 23:35:24 +0100

    update implementation

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -111,8 +111,7 @@
                                 <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80"
                                     data-aos="flip-left">
                                     <div class="relative">
-                                        <img src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                                            alt=""
+                                        <img src="@/assets/img/hero1.jpg" alt=""
                                             class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                                             data-aos="flip-left">
                                         <div
@@ -122,8 +121,7 @@
                                 </div>
                                 <div class="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                                     <div class="relative" data-aos="flip-left">
-                                        <img src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                                            alt=""
+                                        <img src="@/assets/img/hero2.jpg" alt=""
                                             class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                                             data-aos="flip-left">
                                         <div
@@ -131,8 +129,7 @@
                                         </div>
                                     </div>
                                     <div class="relative" data-aos="flip-left">
-                                        <img src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
-                                            alt=""
+                                        <img src="@/assets/img/hero3.jpg" alt=""
                                             class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                                             data-aos="flip-left">
                                         <div
@@ -142,7 +139,7 @@
                                 </div>
                                 <div class="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                                     <div class="relative" data-aos="flip-left">
-                                        <img src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
+                                        <img src="@/assets/img/hero4.jpg"
                                             alt=""
                                             class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                                             data-aos="flip-left">
@@ -151,7 +148,7 @@
                                         </div>
                                     </div>
                                     <div class="relative" data-aos="flip-left">
-                                        <img src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                                        <img src="@/assets/img/hero5.jpg"
                                             alt=""
                                             class="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                                             data-aos="flip-left">
